bffc04a21cd9625d0f6af4c74ca6627369801592
Don <496156+nikolat@users.noreply.github.com>
2025-09-21 09:22:53 +0900

    replace profile relays

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,7 @@ export const reactionEventKind = 17;
 export const expansionThreshold = 5;
 export const urlToLinkEvent = 'https://njump.me';
 export const defaultRelays = ['wss://relay.nostr.band/', 'wss://nos.lol/', 'wss://relay.damus.io/'];
-export const profileRelays = ['wss://purplepag.es/', 'wss://directory.yabu.me/'];
+export const profileRelays = ['wss://user.kindpag.es/', 'wss://directory.yabu.me/'];
 export const defaultReaction = '⭐';
 export const getRoboHashURL = (pubkey: string) => {
 	return `https://robohash.org/${npubEncode(pubkey)}?set=set4`;
